0c0b5c09e5478b41c2555d872721cdb918530ecb
waniven <waniven@icloud.com>
2024-06-21 16:34:58 +1200

    routing for all main pages as well as account and profile pages has been set up

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeContextProvider } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
