8439f6e813e1b34c0d674710145b8c3306d2cba4
waniven <waniven@icloud.com>
2024-07-17 22:50:34 +1200

    got data to load, but broke the sites visuals

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using Repositories; 
+using Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Sets development environment to use in-memory database
-if (builder.Environment.IsDevelopment()){
+if (builder.Environment.IsDevelopment())
+{
     builder.Services.AddDbContext<LunchContext>(options =>
         options.UseInMemoryDatabase("Lunch"));
 
@@ -14,7 +15,9 @@ if (builder.Environment.IsDevelopment()){
 
     builder.Services.AddDbContext<StaffContext>(options =>
         options.UseInMemoryDatabase("Staff"));
-}else{
+}
+else
+{
     builder.Services.AddDbContext<LunchContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("LunchContext") ?? throw new InvalidOperationException("Connection string 'LunchContext' not found.")));
 
@@ -34,15 +37,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()){
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins"); // Add the CORS middleware
 
 app.UseAuthorization();
 
